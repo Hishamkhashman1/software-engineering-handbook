ccbcf81519c21e3e057a49f70c0cfbb6947d9bb8
Hisham <hisham.khashman@gmail.com>
2026-08-04 10:46:40 +0300

    adding more cases

--- test.c
+++ test.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+// int main() {
+// 	printf("hello, world");
+// 	return 0;
+// }
+
 int main() {
-	printf("hello, world");
+	int myList[3] = {1,2,3};
+
+	printf("2nd element: %d\n", myList[1]);
+
 	return 0;
 }
-
--- test.py
+++ test.py
@@ -7,5 +7,87 @@ b = copy.deepcopy(a)
 
 b[0].append(4)
 
-print (b)
-print (a)
+#print (b)
+#print (a)
+
+def test_items(item, items=None):
+    if items is None:
+        items = []
+
+    items.append(item)
+    return items
+
+# Exercise 1 — Generate Even Numbers
+#
+# Write a function called:
+#
+# generate_even(limit)
+#
+# Requirements:
+#
+# It should generate only even numbers.
+# Starting from 0.
+# Until limit.
+# Use a generator.
+# Do not return a list.
+#
+# I want to be able to do:
+#
+# for n in generate_even(10):
+#     print(n)
+#
+# Output:
+#
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
+# Rules
+# Don't Google.
+# Don't worry if you don't remember yield.
+# Just write what you think it should look like.
+#
+# When you're done, paste the code here and we'll discuss it together.
+#
+# Then we'll make it progressively harder:
+#
+# Fibonacci generator
+# Read a huge log file lazily
+# Simulate a paginated API
+# Build a generator that streams AI responses (very close to what LLMs do)
+#
+def generate_even(limit):
+    for n in range(limit):
+        if n % 2 == 0:
+            yield n
+
+for n in generate_even(10):
+    print(n)
+
+def generate_even_numbers(limit_1):
+    even_list = []
+
+    for n in range(limit_1):
+        if n % 2 == 0:
+            even_list.append(n)
+    return even_list
+
+print (generate_even_numbers(10))
+
+
+def decorator(func):
+    def wrapper():
+        print("I said hola como estas")
+        print("she said")
+        func()
+    return wrapper
+
+@decorator
+def test_hello():
+    print("こんにちは")
+
+#test_hello()
+
+print(test_hello)
