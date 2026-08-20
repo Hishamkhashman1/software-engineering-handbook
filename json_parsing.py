a752d645e4c63d73cd463a130088722955d8fa4a
Hisham <hisham.khashman@gmail.com>
2026-08-20 08:48:55 +0300

    updating

--- json_parsing.py
+++ json_parsing.py
@@ -11,4 +11,19 @@ json_data = '''
 
 json_input = loads(json_data)
 
+print (json_input)
+
+def solution(json_input):
+  result = {}
+  
+  if json_input.get("active") == True:
+    result["user_id"] = json_input["id"]
+    result["username"] = json_input["name"]
+    result["status"] = json_input["active"]
+
+  return result
+
+
+
+print (solution(json_input))
 
