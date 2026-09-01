f695289d2f158038d3420c7e59b5cacb7ba9c8b4
Hisham Khashman <hisham.khashman@gmail.com>
2026-09-01 17:29:55 +0900

    added new mondai

---
+++ merge_sorted_array.py
@@ -0,0 +1,7 @@
+# given two integer arrays nums1 and nums2
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order
+#
+# Example 1 
+#
+# nums1 =  [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# output [1,2,2,3,5,6]
