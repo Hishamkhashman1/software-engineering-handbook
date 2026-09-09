74ceda165ffb2382c25a050972202f00d474c05c
Hisham <hisham.khashman@gmail.com>
2026-09-09 18:47:00 +0300

    added space

--- first_c.c
+++ first_c.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello,\n\tWorld!"); // making a one liner comment
+	printf("Hello, World!"); // making a one liner comment
 	int programmer_age = 39;
 
-	printf("%d", programmer_age);
+	printf("\n%d\n", programmer_age);
 
 	char programmer_name[] = "Hisham";
 
 
-	printf("%s", programmer_name);
+	printf("\n%s\n", programmer_name);
 
 	long id_number = 1980003456;
 
-	printf("%ld", id_number);
+	printf("\n%ld\n", id_number);
 
 	float pi_var = 3.14159265358979;
 
-	printf("%f", pi_var);
+	printf("\n%f\n", pi_var);
 
 	char gender = 'M';
 
-	printf("%c", gender);
+	printf("\n%c\n", gender);
 
 	_Bool is_active_programmer = 1; // bool is obviously true or false from boolean .. but truthy and falsy are binary lol 0 false 1 true treated as %d
 
-	printf("%d", is_active_programmer);
+	printf("\n%d\n", is_active_programmer);
 	return 0; /* making a multiliner
 		     comment */
 }
