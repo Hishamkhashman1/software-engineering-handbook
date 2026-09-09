b5b30b7cd2dcf412145b7ec5654a997cfed3344e
Hisham <hisham.khashman@gmail.com>
2026-09-09 15:26:24 +0300

    updating

--- first_c.c
+++ first_c.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello,\n\tWorld!");
-	return 0;
+	printf("Hello,\n\tWorld!"); // making a one liner comment
+	int programmer_age = 38;
+	return programmer_age;
+	// printf(programmer_age);
+	return 0; /* making a multiliner
+		     comment */
 }
